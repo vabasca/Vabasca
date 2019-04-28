22d2aaf132d6c0a5179d757833628bcdff98af2b
tringuyen2014 <tringuyen2014@fau.edu>
2019-04-28 19:41:11 -0400

    app update

--- Vabasca/app/src/main/java/com/vabasca/MainActivity.java
+++ Vabasca/app/src/main/java/com/vabasca/MainActivity.java
@@ -3,6 +3,8 @@ package com.vabasca;
 import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.os.Bundle;
 import android.util.Log;
 
