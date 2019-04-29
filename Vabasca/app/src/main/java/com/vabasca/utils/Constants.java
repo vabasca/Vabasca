8e52054861f61aae70cb24511160a25edc3c1ca6
tringuyen2014 <tringuyen2014@fau.edu>
2019-04-28 23:27:47 -0400

    app update

--- Vabasca/app/src/main/java/com/vabasca/utils/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.vabasca.utils;
-
-public class Constants {
-
-    public static final String BASE_URL = "http://10.0.2.2:8080/api/v1/";
-    public static final String ID = "id";
-    public static final String USERNAME = "username";
-    public static final String EMAIL = "email";
-}
---
+++ Vabasca/app/src/test/java/com/vabasca/bean/ShoppingListTest.java
@@ -0,0 +1,32 @@
+package com.vabasca.bean;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShoppingListTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getDate() {
+    }
+
+    @Test
+    public void setDate() {
+    }
+}
